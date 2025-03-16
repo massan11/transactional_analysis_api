48e04ce4c1a84f8b354dc2d9976912827b82fa4f
massan11 <mhassan.zoho@gmail.com>
2025-03-16 14:12:07 +0330

    Add users router to main application

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from .database import engine, Base
+from .routes import users
 
 app = FastAPI()
 
 # Create tables
 Base.metadata.create_all(bind=engine)
+
+app.include_router(users.router)
 
 @app.get("/")
 def read_root():
